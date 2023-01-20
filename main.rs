b00d0c4f889144dc2ff80497ca3906f85fc0d7a0
Nellie Zhang <nelliezh@gmail.com>
2023-01-20 18:14:26 -0500

    Put the duplicate detection back in, now works with threading

--- main.rs
+++ main.rs
@@ -1,8 +1,7 @@
+use std::sync::Mutex;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::env;
-use std::thread;
 use itertools::Itertools;
 use num_cpus;
 
@@ -11,9 +10,11 @@ fn main() -> io::Result<()> {
     let cpus = num_cpus::get();
     let file = File::open("words.txt")?;
     let reader = BufReader::new(file);
+
+    // Create a map from word length to a set of words with that length.
     let mut words:HashMap<usize, HashSet<String>> = HashMap::new();
 
-    // Read all words into a map<word_length, set<words>>
+    // Read all words into the map.
     for line in reader.lines() {
         let l = line?.to_lowercase();
         if let Some(words_with_len) = words.get_mut(&l.len()) {
@@ -25,28 +26,44 @@ fn main() -> io::Result<()> {
         };
     }
 
-    // Iterate over arguments
-    for arg in env::args().skip(1) {
+    // Iterate over arguments, skipping the first, which is the program name.
+    for arg in std::env::args().skip(1) {
         // Get the words of the right length
         let words_of_right_length = &words[&arg.len()];
 
+        // Create a map to track what words have already been found.
+        let seen_words = Mutex::new(HashSet::new());
+
+        // Create a set of scoped threads to parallelize the searching work.
         std::thread::scope(|s| {
             let arg = &arg;
-             for thread_index in 0..cpus {
+            let seen_words = &seen_words;
+            // Create one thread for each cpu.
+            for thread_index in 0..cpus {
                 s.spawn(move || {
-                    // Set up a set for detecting duplicates.
-            //        let mut seen_words = HashSet::new();
-
-                    // Iterate over all permutations and see if the permutation is present in the dictionary
+                    // Iterate over all permutations and see if the permutation is present in the dictionary.
+                    // For thread X, we skip X elements at the beginning, and from there on, we
+                    // skip by the number of cpus, so if there are 3 cpus:
+                    // thread 0 takes 0, 3, 6, 9, ...
+                    // thread 1 takes 1, 4, 7, 10, ...
+                    // thread 2 takes 2, 5, 8, 11, ...
                     for p in arg.chars().permutations(arg.len()).skip(thread_index).step_by(cpus) {
-            //            if seen_words.contains(&p) {
-            //                continue;
-            //            }
+                        // Collect the letters back into a word.
                         let perm_word = p.iter().collect::<String>();
                         if words_of_right_length.contains(&perm_word) {
+                            // Found a word that matches. See if it was already seen.
+                            {
+                                let mut locked_map = seen_words.lock().unwrap();
+                                // It was, just skip it.
+                                if locked_map.contains(&p) {
+                                    continue;
+                                }
+                                // If we got here, it's a new word. Add it to the map, then print
+                                // it out.
+                                locked_map.insert(p);
+                            }
                             println!("{}", &perm_word);
                         }
-            //            seen_words.insert(p);
                     }
                 });
             }
