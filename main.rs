b539f10b532cf42c4fbafcce3663de78e10f7110
Nellie Zhang <nelliezh@gmail.com>
2023-01-26 18:40:33 -0500

    After processing command line arguments, start taking input from stdin. This allows the dictionary to not be reloaded, so speeds up solving a bunch of words in a row.

--- main.rs
+++ main.rs
@@ -1,9 +1,55 @@
 use std::sync::Mutex;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self, prelude::*, BufReader, BufRead};
 use itertools::Itertools;
 use num_cpus;
+
+fn solve(arg : &String, cpus: usize, words:&HashMap<usize, HashSet<String>> ) {
+    // Get the words of the right length
+    let words_of_right_length = &words[&arg.len()];
+
+    // Create a map to track what words have already been found.
+    let seen_words = Mutex::new(HashSet::new());
+
+    // Create a set of scoped threads to parallelize the searching work.
+    println!("--------");
+    std::thread::scope(|s| {
+        let arg = &arg;
+        let seen_words = &seen_words;
+        // Create one thread for each cpu.
+        for thread_index in 0..cpus {
+            s.spawn(move || {
+                // Iterate over all permutations and see if the permutation is present in the dictionary.
+                // For thread X, we skip X elements at the beginning, and from there on, we
+                // skip by the number of cpus, so if there are 3 cpus:
+                // thread 0 takes 0, 3, 6, 9, ...
+                // thread 1 takes 1, 4, 7, 10, ...
+                // thread 2 takes 2, 5, 8, 11, ...
+                for p in arg.chars().permutations(arg.len()).skip(thread_index).step_by(cpus) {
+                    // Collect the letters back into a word.
+                    let perm_word = p.iter().collect::<String>();
+                    if words_of_right_length.contains(&perm_word) {
+                        // Found a word that matches. See if it was already seen.
+                        {
+                            let mut locked_map = seen_words.lock().unwrap();
+                            // It was, just skip it.
+                            if locked_map.contains(&p) {
+                                continue;
+                            }
+                            // If we got here, it's a new word. Add it to the map, then print
+                            // it out.
+                            locked_map.insert(p);
+                        }
+                        println!("{}", &perm_word);
+                    }
+                }
+            });
+        }
+    });
+
+    println!("--------");
+}
 
 fn main() -> io::Result<()> {
     // Open the dictionary
@@ -29,46 +75,12 @@ fn main() -> io::Result<()> {
 
     // Iterate over arguments, skipping the first, which is the program name.
     for arg in std::env::args().skip(1) {
-        // Get the words of the right length
-        let words_of_right_length = &words[&arg.len()];
+        solve(&arg, cpus, &words);
+    }
 
-        // Create a map to track what words have already been found.
-        let seen_words = Mutex::new(HashSet::new());
-
-        // Create a set of scoped threads to parallelize the searching work.
-        std::thread::scope(|s| {
-            let arg = &arg;
-            let seen_words = &seen_words;
-            // Create one thread for each cpu.
-            for thread_index in 0..cpus {
-                s.spawn(move || {
-                    // Iterate over all permutations and see if the permutation is present in the dictionary.
-                    // For thread X, we skip X elements at the beginning, and from there on, we
-                    // skip by the number of cpus, so if there are 3 cpus:
-                    // thread 0 takes 0, 3, 6, 9, ...
-                    // thread 1 takes 1, 4, 7, 10, ...
-                    // thread 2 takes 2, 5, 8, 11, ...
-                    for p in arg.chars().permutations(arg.len()).skip(thread_index).step_by(cpus) {
-                        // Collect the letters back into a word.
-                        let perm_word = p.iter().collect::<String>();
-                        if words_of_right_length.contains(&perm_word) {
-                            // Found a word that matches. See if it was already seen.
-                            {
-                                let mut locked_map = seen_words.lock().unwrap();
-                                // It was, just skip it.
-                                if locked_map.contains(&p) {
-                                    continue;
-                                }
-                                // If we got here, it's a new word. Add it to the map, then print
-                                // it out.
-                                locked_map.insert(p);
-                            }
-                            println!("{}", &perm_word);
-                        }
-                    }
-                });
-            }
-        });
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        solve(&line.unwrap(), cpus, &words);
     }
 
     Ok(())
